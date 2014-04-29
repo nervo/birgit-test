cba6e1602e4fa0c6c7b778ccf4a52fdff3848f26
Florian Rey <florian.rey@elao.com>
2014-04-29 17:31:30 +0200

    Oooouh

---
+++ src/Birgit/Test/Trout.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Birgit\Test;
+
+/**
+ * Trout
+ */
+class Trout
+{
+    /**
+     * Get true
+     *
+     * @return bool
+     */
+	public function getTrue()
+	{
+		return true;
+	}
+	
+    /**
+     * Get false
+     *
+     * @return bool
+     */
+	public function getFalse()
+	{
+		return false;
+	}
+}
